a6a4f8d156f71209f88805f566c8151f7dcdff3a
Edwar Vargas <edwarjhoel.99@gmail.com>
2022-08-12 23:40:04 -0500

    merge Fabian

--- ecomapp/urls.py
+++ ecomapp/urls.py
@@ -8,10 +8,5 @@ urlpatterns = [
     path("about/",AboutView.as_view(),name="about"),
     path("contact-us/",ContactView.as_view(),name="contact"),
     path("all-products/",AllProductsView.as_view(),name="allproducts"),
-    path("product/<slug:slug>/", ProductDetailView.as_view(), name="productdetail"),
-    path("add-to-cart-<int:pro_id>/", AddToCartView.as_view(), name="addtocart"),
-    path("my-cart/", MyCartView.as_view(), name="mycart"),
-    path("manage-cart/<int:cp_id>/", ManageCartView.as_view(), name="managecart")
-
-
+    path("product/<slug:slug>/", ProductDetailView.as_view(), name="productdetail")
 ]
